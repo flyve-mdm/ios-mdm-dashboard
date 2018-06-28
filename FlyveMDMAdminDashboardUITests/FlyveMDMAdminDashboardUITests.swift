a786654ca8b1701995db08e1093166a3956f273b
Hector Rondon <hrondon@teclib.com>
2018-06-28 16:20:43 -0400

    ci(snapshots): set take snapshots using fastlane
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- FlyveMDMAdminDashboardUITests/FlyveMDMAdminDashboardUITests.swift
+++ FlyveMDMAdminDashboardUITests/FlyveMDMAdminDashboardUITests.swift
@@ -9,6 +9,8 @@
 import XCTest
 
 class FlyveMDMAdminDashboardUITests: XCTestCase {
+    
+    let app = XCUIApplication()
         
     override func setUp() {
         super.setUp()
@@ -18,7 +20,11 @@ class FlyveMDMAdminDashboardUITests: XCTestCase {
         // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
+        
+        app.launchEnvironment = [ "UITest": "1" ]
+        setupSnapshot(app)
+        app.launch()
+        sleep(1)
 
         // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
@@ -28,7 +34,8 @@ class FlyveMDMAdminDashboardUITests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    /// This method take screenshots from fastlane snapshot
+    func testTakeScreenshots() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
