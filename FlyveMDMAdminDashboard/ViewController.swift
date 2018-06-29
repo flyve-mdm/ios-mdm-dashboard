e2a24e4b47046533514ce40c94bfc2021732e1c7
Hector Rondon <hrondon@teclib.com>
2018-06-29 11:02:51 -0400

    docs(header): add license on the header
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- FlyveMDMAdminDashboard/ViewController.swift
+++ FlyveMDMAdminDashboard/ViewController.swift
@@ -1,10 +1,29 @@
-//
-//  ViewController.swift
-//  FlyveMDMAdminDashboard
-//
-//  Created by Hector Rondon on 27/06/18.
-//  Copyright © 2018 Teclib. All rights reserved.
-//
+/*
+ * LICENSE
+ *
+ * ViewController.swift is part of FlyveMDMAdminDashboard
+ *
+ * FlyveMDMAdminDashboard is a subproject of Flyve MDM. Flyve MDM is a mobile
+ * device management software.
+ *
+ * FlyveMDMAdminDashboard is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 3
+ * of the License, or (at your option) any later version.
+ *
+ * FlyveMDMAdminDashboard is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * ------------------------------------------------------------------------------
+ * @author    Hector Rondon <hrondon@teclib.com>
+ * @copyright Copyright © Teclib. All rights reserved.
+ * @license   LGPLv3 https://www.gnu.org/licenses/lgpl-3.0.html
+ * @link      https://github.com/flyve-mdm/ios-mdm-dashboard.git
+ * @link      http://flyve.org/ios-mdm-dashboard
+ * @link      https://flyve-mdm.com
+ * ------------------------------------------------------------------------------
+ */
 
 import UIKit
 
--- ci/scripts/fastlane_config.sh
+++ ci/scripts/fastlane_config.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
 
+# LICENSE
+#
+# fastlane_config.sh is part of FlyveMDMAdminDashboard
+#
+# FlyveMDMAdminDashboard is a subproject of Flyve MDM. Flyve MDM is a mobile
+# device management software.
+#
+# FlyveMDMAdminDashboard is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+#
+# FlyveMDMAdminDashboard is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# ------------------------------------------------------------------------------
+# @author    Hector Rondon <hrondon@teclib.com>
+# @copyright Copyright © Teclib. All rights reserved.
+# @license   LGPLv3 https://www.gnu.org/licenses/lgpl-3.0.html
+# @link      https://github.com/flyve-mdm/ios-mdm-dashboard.git
+# @link      http://flyve.org/ios-mdm-dashboard
+# @link      https://flyve-mdm.com
+# ------------------------------------------------------------------------------
+
 echo ----------- Create Fastlane environment variables ------------
 # Create Fastlane environment variables
 echo FASTLANE_PASSWORD=$FASTLANE_PASSWORD >> .env
