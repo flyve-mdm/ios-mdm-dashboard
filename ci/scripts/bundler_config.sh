8631361e9dc06a26e82083c77748ec75a22fbb79
Hector Rondon <hrondon@teclib.com>
2018-09-21 16:39:29 -0400

    docs(license): update header license
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- ci/scripts/bundler_config.sh
+++ ci/scripts/bundler_config.sh
@@ -1,29 +1,31 @@
 #!/bin/sh
 
-# LICENSE
 #
-# bundler_config.sh is part of FlyveMDMAdminDashboard
+#  LICENSE
+# 
+#  This file is part of Flyve MDM Admin Dashboard for iOS.
 #
-# FlyveMDMAdminDashboard is a subproject of Flyve MDM. Flyve MDM is a mobile
-# device management software.
+#  Admin Dashboard for iOS is a subproject of Flyve MDM.
+#  Flyve MDM is a mobile device management software.
+# 
+#  Flyve MDM is free software: you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License
+#  as published by the Free Software Foundation; either version 3
+#  of the License, or (at your option) any later version.
 #
-# FlyveMDMAdminDashboard is free software: you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 3
-# of the License, or (at your option) any later version.
+#  Flyve MDM is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  -------------------------------------------------------------------
+#  @author    Hector Rondon - <hrondon@teclib.com>
+#  @copyright Copyright Teclib. All rights reserved.
+#  @license   LGPLv3 https://www.gnu.org/licenses/lgpl-3.0.html
+#  @link      https://github.com/flyve-mdm/ios-mdm-dashboard/
+#  @link      http://flyve.org/ios-mdm-dashboard/
+#  @link      https://flyve-mdm.com
+#  -------------------------------------------------------------------
 #
-# FlyveMDMAdminDashboard is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# ------------------------------------------------------------------------------
-# @author    Hector Rondon <hrondon@teclib.com>
-# @copyright Copyright © Teclib. All rights reserved.
-# @license   LGPLv3 https://www.gnu.org/licenses/lgpl-3.0.html
-# @link      https://github.com/flyve-mdm/ios-mdm-dashboard.git
-# @link      http://flyve.org/ios-mdm-dashboard
-# @link      https://flyve-mdm.com
-# ------------------------------------------------------------------------------
 
 echo ----------- Configure bundler ------------
 echo :update_sources: true >> ~/.gemrc
