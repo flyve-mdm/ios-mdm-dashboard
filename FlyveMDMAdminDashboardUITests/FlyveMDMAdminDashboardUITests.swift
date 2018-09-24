6116cd6ec2fb09fc3bc0813d60ce47fea9414abe
Hector Rondon <hrondon@teclib.com>
2018-09-24 10:40:20 -0400

    docs(license): add license header to code files
    Signed-off-by: Hector Rondon <hrondon@teclib.com>

--- FlyveMDMAdminDashboardUITests/FlyveMDMAdminDashboardUITests.swift
+++ FlyveMDMAdminDashboardUITests/FlyveMDMAdminDashboardUITests.swift
@@ -1,28 +1,28 @@
-/*
- * LICENSE
+/**
+ *  LICENSE
+ * 
+ *  This file is part of Flyve MDM Admin Dashboard for iOS.
  *
- * FlyveMDMAdminDashboardUITests.swift is part of FlyveMDMAdminDashboard
+ *  Admin Dashboard for iOS is a subproject of Flyve MDM.
+ *  Flyve MDM is a mobile device management software.
+ * 
+ *  Flyve MDM is free software: you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 3
+ *  of the License, or (at your option) any later version.
  *
- * FlyveMDMAdminDashboard is a subproject of Flyve MDM. Flyve MDM is a mobile
- * device management software.
- *
- * FlyveMDMAdminDashboard is free software: you can redistribute it and/or
- * modify it under the terms of the GNU General Public License
- * as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
- *
- * FlyveMDMAdminDashboard is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * ------------------------------------------------------------------------------
- * @author    Hector Rondon <hrondon@teclib.com>
- * @copyright Copyright © Teclib. All rights reserved.
- * @license   LGPLv3 https://www.gnu.org/licenses/lgpl-3.0.html
- * @link      https://github.com/flyve-mdm/ios-mdm-dashboard.git
- * @link      http://flyve.org/ios-mdm-dashboard
- * @link      https://flyve-mdm.com
- * ------------------------------------------------------------------------------
+ *  Flyve MDM is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *  -------------------------------------------------------------------
+ *  @author    Hector Rondon - <hrondon@teclib.com>
+ *  @copyright Copyright Teclib. All rights reserved.
+ *  @license   LGPLv3 https://www.gnu.org/licenses/lgpl-3.0.html
+ *  @link      https://github.com/flyve-mdm/ios-mdm-dashboard/
+ *  @link      http://flyve.org/ios-mdm-dashboard/
+ *  @link      https://flyve-mdm.com
+ *  ---------------------------------------------------------------------
  */
 
 import XCTest
